4f67a959c29ed112cd36fdadc9d5a31042e7c20b
Prathikshaa <pratsr996@gmail.com>
2018-11-29 22:14:19 -0500

    update_pred.v - tested and commented

--- pc4-sv116-master__branch_pred/update_pred.v
+++ pc4-sv116-master__branch_pred/update_pred.v
@@ -1,14 +1,30 @@
 /**
- * 
- * 
+ * update_pred.v:
+ * Module to update the prediction when branch result is known in the decode stage
+ * **
+ * Prediction - two bit saturating counter
+ * 0 - strongly not taken
+ * 1 - weakly not taken
+ * 2 - weakly taken
+ * 3 - strongly taken
+ * **
+ * Inputs: 
+ * counter_ip - BPT[BHR] which contains the two bit prediction to update
+ * branchResult - result of the branch that determines the new prediction
+ * **
+ * Outputs:
+ * updated_counter - returns the updated prediction
+ * **
  */
 module update_pred(
 	input [1:0] counter_ip,					// receives BPT[BHR] = 0-3 (2'b)
 	input branchResult,						// actual result of branch = taken/not taken as true or false
-	output wire [1:0] updated_counter,
-	output wire inc,
-	output wire dec,
-	output wire [1:0] update);	//	inc if taken, dec if not taken, within the range 0-3
+	output wire [1:0] updated_counter
+	);	//	inc if taken, dec if not taken, within the range 0-3
+	
+	wire inc;
+	wire dec;
+	wire [1:0] update;
 	
 	// wire inc, dec;
 	and b_increment(inc, branchResult, (counter_ip != 2'b11)); // if branch taken and counter not saturated, inc
